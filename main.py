8135fa3da89a3abb9603b9a04a2f09ed0e7e9a5e
Quinten Yearsley <qyearsley@gmail.com>
2026-03-02 18:03:36 -0800

    Add prompt and verbose args

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
-from dotenv import load_dotenv
+import sys
 
+from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("no api key found")
     client = genai.Client(api_key=api_key)
-    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=contents)
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
+
+    metadata = response.usage_metadata
+    if not metadata:
+        print("No reponse metadata")
+        sys.exit(1)
+    prompt_tokens = metadata.prompt_token_count
+    response_tokens = metadata.candidates_token_count
+    if args.verbose:
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
     if not response:
         raise RuntimeError("generate_content no response")
     print(response.text)
