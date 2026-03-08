5810d59ac6a81018bf83e2c001ffea3d60289d8c
Quinten Yearsley <qyearsley@gmail.com>
2026-03-07 17:32:07 -0800

    Add get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,23 @@
+import os
+
+
 def get_files_info(working_directory, directory="."):
-    pass
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    print('Result for current directory:')
+    try:
+        # Will be True or False
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
+        if not valid_target_dir:
+            raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        if (not os.path.isdir(target_dir)):
+            raise Exception(f'Error: "{directory}" is not a directory')
+        for item in os.listdir(target_dir):
+            path = os.path.join(target_dir, item)
+            size = os.path.getsize(path)
+            is_dir = os.path.isdir(path)
+            print(f' - {item}: file_size={size} bytes, is_dir={is_dir}')
+    except Exception as e:
+        print(f'  Error: {e}')
---
+++ test_get_files_info.py
@@ -0,0 +1,13 @@
+from functions.get_files_info import get_files_info
+
+
+def main():
+    get_files_info("calculator", ".")
+    get_files_info("calculator", "pkg")
+    get_files_info("calculator", "/bin")
+    get_files_info("calculator", "../")
+
+
+if __name__ == "__main__":
+    main()
+
